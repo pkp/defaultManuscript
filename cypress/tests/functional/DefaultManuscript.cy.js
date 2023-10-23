03067e8ef1130c14979caa00c08c7f4f53a978b6
Jarda Kotěšovec <jarda.kotesovec@gmail.com>
2023-10-23 12:37:30 +0200

    pkp/defaultManuscript#41 update cypress test

--- cypress/tests/functional/DefaultManuscript.cy.js
+++ cypress/tests/functional/DefaultManuscript.cy.js
@@ -3,27 +3,36 @@
  *
  * Copyright (c) 2014-2020 Simon Fraser University
  * Copyright (c) 2000-2020 John Willinsky
- * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ * Distributed under the GNU GPL v3. For full terms see the file LICENSE.
  *
  */
 
 describe('Theme plugin tests', function() {
 	it('Enables and selects the theme', function() {
-		cy.login('admin', 'admin', 'publicknowledge');
+    cy.login('admin', 'admin', 'publicknowledge');
 
-		cy.get('ul[id="navigationPrimary"] a:contains("Settings")').click();
-		cy.get('ul[id="navigationPrimary"] a:contains("Website")').click();
-		cy.get('button[id="plugins-button"]').click();
+    cy.get('a:contains("Website")').click();
+    cy.get('button[id="plugins-button"]').click();
 
-		// Find and enable the plugin
-		cy.get('input[id^="select-cell-defaultmanuscriptchildthemeplugin-enabled"]').click();
-		cy.get('div:contains(\'The plugin "Manuscript (Default child theme)" has been enabled.\')');
+    // Find and enable the plugin
+    cy.get(
+      'input[id^="select-cell-defaultmanuscriptchildthemeplugin-enabled"]'
+    ).click();
+    cy.get(
+      "div:contains('The plugin \"Manuscript (Default child theme)\" has been enabled.')"
+    );
 
-		// Select the new theme
-		cy.get('button[id="appearance-button"]').click();
-		cy.get('select[id="theme-themePluginPath-control"]').select('Manuscript (Default child theme)');
-		cy.get('div:contains("The theme has been updated.")');
-	});
+    // Appearance tab does not get updated until the reload
+    cy.reload();
+
+    // Select the new theme
+    cy.get('button[id="appearance-button"]').click();
+    cy.get('select[id="theme-themePluginPath-control"]').select(
+      "Manuscript (Default child theme)"
+    );
+
+    cy.get('#theme button:contains("Save")').click();
+  });
 
 	it('Views the theme', function() {
 		cy.visit('');
