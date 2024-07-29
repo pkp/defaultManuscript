72de7116f08a86e4148e81cceebc41faf045e69b
withanage <dulip.withanage@gmail.com>
2024-07-29 22:15:39 +0200

    Add github actions support for main

---
+++ .github/actions/tests.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+set -e
+npx cypress run --spec "cypress/tests/data/10-ApplicationSetup/10-Installation.cy.js,cypress/tests/data/10-ApplicationSetup/20-CreateContext.cy.js"
+npx cypress run  --headless --browser chrome  --config '{"specPattern":["plugins/generic/defaultManuscript/cypress/tests/functional/*.cy.js"]}'
+
