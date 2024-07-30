3249cfc6355590814a2b851192678db85f7755cc
Dulip Withanage <withanage@users.noreply.github.com>
2024-07-31 00:08:07 +0200

    Update tests.sh

--- .github/actions/tests.sh
+++ .github/actions/tests.sh
@@ -2,5 +2,5 @@
 
 set -e
 npx cypress run --spec "cypress/tests/data/10-Installation.spec.js,cypress/tests/data/20-CreateContext.spec.js"
-npx cypress run  --config integrationFolder=plugins/generic/defaultManuscript/cypress/tests
+npx cypress run  --config integrationFolder=plugins/themes/defaultManuscript/cypress/tests
 
