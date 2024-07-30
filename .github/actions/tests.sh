0e430d09060608cf3590a9de22add2e636502233
Dulip Withanage <withanage@users.noreply.github.com>
2024-07-31 00:03:07 +0200

    Update tests.sh

--- .github/actions/tests.sh
+++ .github/actions/tests.sh
@@ -2,6 +2,6 @@
 
 set -e
 npx cypress run --spec "cypress/tests/data/10-ApplicationSetup/10-Installation.cy.js,cypress/tests/data/10-ApplicationSetup/20-CreateContext.cy.js"
-npx cypress run  --headless --browser chrome  --config '{"specPattern":["plugins/generic/customBlockManager/cypress/tests/functional/*.cy.js"]}'
+npx cypress run  --headless --browser chrome  --config '{"specPattern":["plugins/generic/dafaultManuscript/cypress/tests/functional/*.cy.js"]}'
 
 
